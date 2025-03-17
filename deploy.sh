c801ff640bd57a08223a23989b7c977608f7fb7f
ryan-tech <ryank.sdet@gmail.com>
2025-03-17 09:40:28 -0700

    Added git fetch and pull to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
+
 # Define variables
 SERVICE_NAME="greensaucebot"
+# Update code from repository
+git fetch
+git pull
 
 # Build and start the service using docker-compose
 docker-compose up -d --build
