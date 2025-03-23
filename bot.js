b06c377bde44064c91af6a3123d89e2ff06e0006
ryan-tech <ryank.sdet@gmail.com>
2025-03-22 20:16:02 -0700

    fixed command file path

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const { Client, GatewayIntentBits, Events } = require('discord.js');
 require('dotenv').config();
 const TOKEN = process.env.TOKEN;
 console.log(TOKEN)
-const { deployCommands } = require('./Commands/deploy-commands.js');
+const { deployCommands } = require('./Commands/deployCommands.js');
 const { pomodoroHandler } = require('./bot_modules/pomodoro.js');
 const { sayJoke, sayQuote, sayRiddle } = require('./bot_modules/everydayFun.js');
 
